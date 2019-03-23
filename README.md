27a5cfe08b8e7472573555d9efecf7283e91e6fb
Servuc <thomas@servuc.fr>
2019-03-23 02:26:29 +0100

    First commit :)

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Circuidy-map
+
 Map backend for Circuidy app
+
+## Start server
+
+    npm install
+    node server.js
+    
+Server will serve on port 3000
+    
+## How interact with it
+
+Just send POST request like that :
+
+    IP:3000/notifications?lat=12&lon=15&type=3&message=Dommage test
+    
+ - lat : Latitude in decimal
+ - lon : Longitude in decimal
+ - type : Various PIN
+   - DANGER
+   - DUSTBIN
+   - SMILE
+ - message : Message to accompagne notification
---
+++ server.js
@@ -0,0 +1,66 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+const express = require('express');
+var bodyParser     =        require("body-parser");
+const app = express();
+
+var fs = require('fs');
+
+var server = http.createServer(function(request, response) {
+    // process HTTP request. Since we're writing just WebSockets
+    // server we don't have to implement anything.
+});
+server.listen(1337, function() { });
+
+// create the server
+wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+// WebSocket server
+wsServer.on('request', function(request) {
+    var connection = request.accept(null, request.origin);
+    console.log("Client connecté")
+
+    // This is the most important callback for us, we'll handle
+    // all messages from users here.
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            // process WebSocket message
+        }
+    });
+
+    connection.on('close', function(connection) {
+        console.log("Client déconnecté")
+    });
+});
+
+app.get('/', function (req, res) {
+    fs.readFile('static/index.html', 'utf8', function(err, contents) {
+        res.send( contents );
+    });
+});
+
+app.post('/notifications', function (req, res) {
+    console.log( req.query );
+    if( req.query.type !== undefined && req.query.message !== undefined && req.query.lat !== undefined && req.query.lon !== undefined ) {
+        if( wsServer !== undefined ) {
+            wsServer.broadcast( JSON.stringify({
+                type: req.query.type,
+                message: req.query.message,
+                lat: req.query.lat,
+                lon: req.query.lon,
+                date: Date.now().toString()
+            }))
+            res.status(200).end();
+        }
+    }
+    res.status(400).end();
+});
+
+app.use('/public', express.static(__dirname + '/node_modules'))
+app.use('/static', express.static(__dirname + '/static'))
+app.listen(3000, function () {
+    console.log('Running !')
+})
